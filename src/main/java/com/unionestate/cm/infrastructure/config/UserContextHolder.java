356239ae12a38760f6b0f6456cc09674a2aa1daf
Jorge Torres <jtorres@highroads.com>
2024-08-17 16:55:58 +0100

    Adding genericcontroller, usercontext and authorizationmanager

---
+++ src/main/java/com/unionestate/cm/infrastructure/config/UserContextHolder.java
@@ -0,0 +1,9 @@
+package com.unionestate.cm.infrastructure.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserContextHolder {
+    
+}
--- src/main/java/com/unionestate/cm/web/controller/GenericController.java
+++ src/main/java/com/unionestate/cm/web/controller/GenericController.java
@@ -1,5 +1,12 @@
 package com.unionestate.cm.web.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
 public class GenericController {
-    
+    //private final AuthorizationManager authorizationManager; // use dependency-graph and roles-permissions.json file for REST permissions
 }
