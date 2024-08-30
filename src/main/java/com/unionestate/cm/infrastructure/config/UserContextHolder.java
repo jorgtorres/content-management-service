fabf05f4787eea6ddf93e7d825b7218f6e7abb33
Jorge Torres <jtorres@highroads.com>
2024-08-30 14:55:32 +0100

    Adding UserContext and UserToken

--- src/main/java/com/unionestate/cm/infrastructure/config/UserContextHolder.java
+++ src/main/java/com/unionestate/cm/infrastructure/config/UserContextHolder.java
@@ -1,9 +1,16 @@
 package com.unionestate.cm.infrastructure.config;
 
+import com.unionestate.commons.sso.UserToken;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserContextHolder {
-    
+
+    private static final ThreadLocal<UserToken> CONTEXT_THREAD = ThreadLocal.withInitial(() -> {
+        UserToken defaultUserToken = new UserToken();
+        defaultUserToken.setUsername("defaultUserToken");
+        defaultUserToken.setOrganization("public");
+        return defaultUserToken;
+    });
 }
