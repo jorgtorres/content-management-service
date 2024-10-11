bdcfb2bbfd7068419d57023e59b3eb3b44f044bd
Jorge Torres <jorgevnt20@gmail.com>
2024-10-11 16:32:22 -0500

    Add local connection WIP

--- src/main/java/com/unionestate/cm/CmApplication.java
+++ src/main/java/com/unionestate/cm/CmApplication.java
@@ -1,5 +1,12 @@
 package com.unionestate.cm;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -8,16 +15,19 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.util.TimeZone;
 
@@ -26,6 +36,7 @@ import java.util.TimeZone;
                 RedisAutoConfiguration.class,
                 RedisRepositoriesAutoConfiguration.class,
                 JpaRepositoriesAutoConfiguration.class,
+                DataSourceAutoConfiguration.class
         }
 )
 @SecurityScheme(
@@ -63,5 +74,18 @@ public class CmApplication {
     @Bean
     public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
         return factory -> factory.setContextPath(contextPath);
+    }
+
+    @Bean
+    @Qualifier("rdsResponseMapper")
+    public ObjectMapper getRdsResponseMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.serializationInclusion(JsonInclude.Include.NON_NULL)
+                .featuresToEnable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .featuresToDisable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+
+                .modules(new ParameterNamesModule(), new Jdk8Module(), new JavaTimeModule())
+                .indentOutput(true)
+                .build();
     }
 }
