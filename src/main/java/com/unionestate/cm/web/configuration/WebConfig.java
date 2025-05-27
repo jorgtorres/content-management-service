d5b293d83120709ad5b06d7d99dab41b82bad814
Jorge Torres <jtorres@highroads.com>
2025-05-26 21:46:11 -0500

    add formatter to @Web config

--- src/main/java/com/unionestate/cm/web/configuration/WebConfig.java
+++ src/main/java/com/unionestate/cm/web/configuration/WebConfig.java
@@ -1,9 +1,12 @@
 package com.unionestate.cm.web.configuration;
 
 import com.unionestate.cm.web.common.RequestInterceptor;
+import com.unionestate.commons.model.UestCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +22,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(requestInterceptor).excludePathPatterns(apiDocUrl, "/v3/api-docs/**", "/swagger-ui/**");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new UestCollectionConverter());
+    }
+
+    private class UestCollectionConverter implements Converter<String, UestCollection> {
+
+        @Override
+        public UestCollection convert(String source) {
+            return UestCollection.forValue(source);
+        }
+
     }
 }
